5b8f3e4b14dfd84ae11bdb62c5dc92476646352c
Eugene Rozhkov <eugene.rozhkov@betssongroup.com>
2025-03-10 16:03:50 +0200

    Trapping rain water

---
+++ Sandbox.Leetcode/42_TrappingRainWater.cs
@@ -0,0 +1,60 @@
+﻿using System.Diagnostics;
+
+namespace Sandbox.Leetcode;
+
+public class _42_TrappingRainWater
+{
+    [Theory]
+    [InlineData(new int[] { 0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1 }, 6)]
+    [InlineData(new int[] { 4, 2, 0, 3, 2, 5 }, 9)]
+    public void Trap(int[] height, int expected)
+    {
+        Debug.WriteLine($"[{string.Join(", ", height)}] => {expected}");
+        Assert.Equal(expected, GetTrappedRainWater(height));
+    }
+
+    public int GetTrappedRainWater(int[] height)
+    {
+        int highestIndex = 0;
+        int highest = 0;
+
+        for (int i = 0; i < height.Length; i++)
+        {
+            if (height[i] > highest)
+            {
+                highest = height[i];
+                highestIndex = i;
+            }
+        }
+
+        int trapped = 0;
+        highest = 0;
+
+        for (int i = 0; i < highestIndex; i++)
+        {
+            if (height[i] > highest)
+            {
+                highest = height[i];
+            }
+            else
+            {
+                trapped += highest - height[i];
+            }
+        }
+
+        highest = 0;
+        for (int i = height.Length - 1; i > highestIndex; i--)
+        {
+            if (height[i] > highest)
+            {
+                highest = height[i];
+            }
+            else
+            {
+                trapped += highest - height[i];
+            }
+        }
+
+        return trapped;
+    }
+}
