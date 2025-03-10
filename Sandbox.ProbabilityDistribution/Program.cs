52bb20dd74b8faac898b0bf9238a3043c3902578
Eugene Rozhkov <eugene.rozhkov@betssongroup.com>
2025-03-10 10:16:12 +0200

    Add project files.

---
+++ Sandbox.ProbabilityDistribution/Program.cs
@@ -0,0 +1,170 @@
+﻿using MathNet.Numerics.RootFinding;
+using Microsoft.Extensions.Configuration;
+using System.Text.Json;
+
+namespace Sandbox.ProbabilityDistribution
+{
+    internal class Program
+    {
+        static double _targetRtp = 0.97;
+
+        static void Main(string[] args)
+        {
+            var config = new ConfigurationBuilder()
+                .AddUserSecrets<Program>()
+                .Build();
+
+            var games = JsonSerializer.Deserialize<double[][]>(config["Games"]);
+
+            Console.WriteLine($"Target RTP: {_targetRtp.ToString("P2")}");
+
+            //Calculate the weights configuration for each game setup by adjusting the probability distribution steepness
+            foreach (var game in games)
+            {
+                Console.WriteLine();
+                CalculateConfig(game, _targetRtp);
+            }
+        }
+
+        static void CalculateConfig(double[] buckets, double targetRtp)
+        {
+            //Find the maximum steepness that will result in an RTP lesser than the target RTP
+            double maxSteepness = 0;
+            while (CalculateRtp(buckets, maxSteepness) > targetRtp) maxSteepness++;
+
+            //Find the steepness that will result in an RTP equal to the target RTP using Brent's method
+            Func<double, double> f = x => CalculateRtp(buckets, x);
+            var targetSteepness = Brent.FindRoot(x => f(x) - targetRtp, 0, maxSteepness);
+
+            //Calcualte the weights using the target steepness
+            var weights = CalculateWeights(buckets, targetSteepness);
+
+            //Make the weights integers and adjust them to match the target RTP
+            var refinedWeights = RefineWeights(weights);
+            var adjustedWeights = AdjustWeights(buckets, refinedWeights, targetRtp);
+
+            //Print the results
+            Console.WriteLine($"Buckets: {string.Join(", ", buckets)}");
+            Console.WriteLine($"Weights: {string.Join(", ", adjustedWeights)}");
+            Console.WriteLine($"Expected RTP: {CalculateRtp(buckets, adjustedWeights).ToString("P2")}");
+        }
+
+        static double[] CalculateArguments(double[] multipliers)
+        {
+            var arguments = new double[multipliers.Length];
+
+            var maxIndex = multipliers.Length - 1;
+            var midIndex = maxIndex / 2;
+
+            for (int i = 0; i < multipliers.Length; i++)
+            {
+                arguments[i] = (double)(i - midIndex - (i > midIndex ? maxIndex % 2 : 0)) / midIndex;
+            }
+
+            return arguments;
+        }
+
+        static double[] CalculateWeights(double[] multipliers, double steepness)
+        {
+            var weights = new double[multipliers.Length];
+            var arguments = CalculateArguments(multipliers);
+            for (int i = 0; i < multipliers.Length; i++)
+            {
+                weights[i] = Math.Pow(Math.E, -Math.Pow(arguments[i], 2) * steepness);
+            }
+            return weights;
+        }
+
+        static double CalculateRtp(double[] multipliers, double steepness)
+        {
+            var weights = CalculateWeights(multipliers, steepness);
+
+            return CalculateRtp(multipliers, weights);
+        }
+
+        static double CalculateRtp(double[] multipliers, double[] weights)
+        {
+            double expectation = 0;
+
+            for (int i = 0; i < multipliers.Length; i++)
+            {
+                expectation += multipliers[i] * weights[i];
+            }
+
+            return expectation / weights.Sum();
+        }
+
+        static double[] RefineWeights(double[] weights)
+        {
+            var refinedWeights = new double[weights.Length];
+            var minWeight = weights.Min();
+            for (int i = 0; i < weights.Length; i++)
+            {
+                refinedWeights[i] = Math.Round(weights[i] / minWeight);
+            }
+            return refinedWeights;
+        }
+
+        static double[] AdjustWeights(double[] multipliers, double[] weights, double targetRtp)
+        {
+            var maxIndex = multipliers.Length - 1;
+            var midIndex = maxIndex / 2;
+
+            while (CalculateRtp(multipliers, weights) > targetRtp) weights[midIndex]++;
+
+            while (CalculateRtp(multipliers, weights) < targetRtp) weights[midIndex]--;
+
+            var weight0 = weights[midIndex];
+            var rtp0 = CalculateRtp(multipliers, weights);
+
+            weights[midIndex]++;
+
+            var weight1 = weights[midIndex];
+            var rtp1 = CalculateRtp(multipliers, weights);
+
+            var midWeight = LinearInterpolation(rtp0, weight0, rtp1, weight1, targetRtp);
+
+            if (multipliers.Length % 2 == 0)
+            {
+                midWeight = (midWeight + weights[midIndex + 1]) / 2;
+
+                weights[midIndex] = midWeight;
+                weights[midIndex + 1] = midWeight;
+            }
+            else
+            {
+                weights[midIndex] = midWeight;
+            }
+
+            for (int i = 0; i < weights.Length; i++)
+            {
+                weights[i] = Math.Round(weights[i] * 100);
+            }
+
+            var gcd = weights.Select(w => (long)w).Aggregate(GCD);
+
+            for (int i = 0; i < weights.Length; i++)
+            {
+                weights[i] /= gcd;
+            }
+
+            return weights;
+        }
+
+        static double LinearInterpolation(double x0, double y0, double x1, double y1, double x)
+        {
+            return y0 + (y1 - y0) / (x1 - x0) * (x - x0);
+        }
+
+        static long GCD(long a, long b)
+        {
+            while (b != 0)
+            {
+                long temp = b;
+                b = a % b;
+                a = temp;
+            }
+            return Math.Abs(a); // Ensure positive GCD
+        }
+    }
+}
---
+++ Sandbox.SudokuSolver/Program.cs
@@ -0,0 +1,414 @@
+﻿using System.Reflection;
+
+namespace Sandbox.SudokuSolver;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        char[][] board = [
+            ['5', '3', '.', '.', '7', '.', '.', '.', '.'],
+            ['6', '.', '.', '1', '9', '5', '.', '.', '.'],
+            ['.', '9', '8', '.', '.', '.', '.', '6', '.'],
+            ['8', '.', '.', '.', '6', '.', '.', '.', '3'],
+            ['4', '.', '.', '8', '.', '3', '.', '.', '1'],
+            ['7', '.', '.', '.', '2', '.', '.', '.', '6'],
+            ['.', '6', '.', '.', '.', '.', '2', '8', '.'],
+            ['.', '.', '.', '4', '1', '9', '.', '.', '5'],
+            ['.', '.', '.', '.', '8', '.', '.', '7', '9']
+            ];
+
+
+        SolveSudoku(board);
+
+        Console.WriteLine(string.Join(Environment.NewLine, board.Select(row => string.Join(' ', row))));
+    }
+
+    public static void SolveSudoku(char[][] board)
+    {
+        var bv = SolveBoard(board);
+        if (bv != null)
+        {
+            bv.FillSolution(board);
+        }
+    }
+
+    private static BoardVolatile SolveBoard(char[][] board)
+    {
+        int solutionLength = 0;
+        var queue = new Queue<List<char>>();
+
+        var bv = new BoardVolatile(board);
+        bv.CheckBoard();
+
+        //Find rows, columns and sets, where any of the values is possible only on one place, place it there and repeat
+        int optionsSet;
+
+        do
+        {
+            optionsSet = 0;
+
+            var possiblePositions = new Dictionary<char, List<(int, int)>>();
+            //Rows
+            for (int i = 0; i < 9; i++)
+            {
+                possiblePositions.Clear();
+
+                for (int j = 0; j < 9; j++)
+                {
+                    var opions = bv.GetOptions(i, j);
+                    if (opions.Count == 1) continue;
+
+                    foreach (var option in opions)
+                    {
+                        if (possiblePositions.ContainsKey(option))
+                        {
+                            possiblePositions[option].Add((i, j));
+                        }
+                        else
+                        {
+                            possiblePositions[option] = new List<(int, int)> { (i, j) };
+                        }
+                    }
+                }
+
+                foreach (var item in possiblePositions.Where(p => p.Value.Count == 1))
+                {
+                    bv.SetOption(item.Value.First().Item1, item.Value.First().Item2, item.Key);
+                    optionsSet++;
+                }
+                bv.CheckBoard();
+            }
+
+            //Columns
+            for (int j = 0; j < 9; j++)
+            {
+                possiblePositions.Clear();
+
+                for (int i = 0; i < 9; i++)
+                {
+                    var opions = bv.GetOptions(i, j);
+                    if (opions.Count == 1) continue;
+
+                    foreach (var option in opions)
+                    {
+                        if (possiblePositions.ContainsKey(option))
+                        {
+                            possiblePositions[option].Add((i, j));
+                        }
+                        else
+                        {
+                            possiblePositions[option] = new List<(int, int)> { (i, j) };
+                        }
+                    }
+                }
+
+                foreach (var item in possiblePositions.Where(p => p.Value.Count == 1))
+                {
+                    bv.SetOption(item.Value.First().Item1, item.Value.First().Item2, item.Key);
+                    optionsSet++;
+                }
+                bv.CheckBoard();
+            }
+
+            //Sets
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    possiblePositions.Clear();
+
+                    for (int k = 0; k < 3; k++)
+                    {
+                        for (int l = 0; l < 3; l++)
+                        {
+                            var opions = bv.GetOptions(i * 3 + k, j * 3 + l);
+                            if (opions.Count == 1) continue;
+
+                            foreach (var option in opions)
+                            {
+                                if (possiblePositions.ContainsKey(option))
+                                {
+                                    possiblePositions[option].Add((i * 3 + k, j * 3 + l));
+                                }
+                                else
+                                {
+                                    possiblePositions[option] = new List<(int, int)> { (i * 3 + k, j * 3 + l) };
+                                }
+                            }
+                        }
+                    }
+
+                    foreach (var item in possiblePositions.Where(p => p.Value.Count == 1))
+                    {
+                        bv.SetOption(item.Value.First().Item1, item.Value.First().Item2, item.Key);
+                        optionsSet++;
+                    }
+                    bv.CheckBoard();
+                }
+            }
+        } while (optionsSet > 0);
+
+        var volatility = bv.GetVolatility();
+
+        if (volatility == 0) return null;
+        
+        if (volatility == 1) return bv;
+        
+        //Solving by traversing options tree
+        foreach (var o in bv.GetFirstOptions()) queue.Enqueue(new List<char> { o });
+        solutionLength = 1;
+
+        while (queue.Count > 0)
+        {
+            var solution = queue.Dequeue();
+
+            if (solution.Count > solutionLength)
+            {
+                solutionLength = solution.Count;
+                Console.WriteLine($"Solution length: {solutionLength}");
+            }
+
+            var iBv = new BoardVolatile(board);
+            iBv.SetOptions(solution);
+            iBv.CalculateOptions();
+            var iVolatility = iBv.GetVolatility();
+
+            if (volatility == 0) continue;
+            else if (volatility == 1) return bv;
+            else
+            {
+                var options = iBv.GetFirstOptions();
+
+                foreach (var o in options)
+                {
+                    var newSolution = new List<char>();
+                    newSolution.AddRange(solution);
+                    newSolution.Add(o);
+                    queue.Enqueue(newSolution);
+                }
+            }
+        }
+
+        return null;
+    }
+}
+
+internal class BoardVolatile
+{
+    List<char>[,] _options;
+
+    char[] digits = new char[] { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+
+    public BoardVolatile(char[][] board)
+    {
+        _options = new List<char>[9, 9];
+        for (int i = 0; i < 9; i++)
+        {
+            for (int j = 0; j < 9; j++)
+            {
+                _options[i, j] = new List<char>();
+                if (board[i][j] != '.') _options[i, j].Add(board[i][j]);
+            }
+        }
+    }
+
+    public int GetVolatility()
+    {
+        for (int i = 0; i < 9; i++)
+        {
+            for (int j = 0; j < 9; j++)
+            {
+                if (_options[i, j].Count == 0) return 0;
+                if (_options[i, j].Count > 1) return 2;
+            }
+        }
+        return 1;
+    }
+
+    public List<char> GetFirstOptions()
+    {
+        for (int i = 0; i < 9; i++)
+        {
+            for (int j = 0; j < 9; j++)
+            {
+                if (_options[i, j].Count > 1) return _options[i, j];
+            }
+        }
+        return new List<char>();
+    }
+
+    public void SetFirstOption(char value)
+    {
+        for (int i = 0; i < 9; i++)
+        {
+            for (int j = 0; j < 9; j++)
+            {
+                if (_options[i, j].Count == 0)
+                {
+                    _options[i, j] = new List<char> { value };
+                    return;
+                }
+            }
+        }
+    }
+
+    public void SetOptions(List<char> values)
+    {
+        foreach (char c in values)
+        {
+            SetFirstOption(c);
+        }
+    }
+
+    public void FillSolution(char[][] board)
+    {
+        if (GetVolatility() != 1) throw new Exception("Solution not found");
+
+        for (int i = 0; i < 9; i++)
+        {
+            for (int j = 0; j < 9; j++)
+            {
+                board[i][j] = _options[i, j].First();
+            }
+        }
+    }
+
+    public void CalculateCellOptions(int x, int y)
+    {
+        var values = new List<char>();
+
+        //Row
+        for (int j = 0; j < 9; j++)
+        {
+            if (_options[x, j].Count == 1) values.Add(_options[x, j].First());
+        }
+
+        //Column
+        for (int i = 0; i < 9; i++)
+        {
+            if (_options[i, y].Count == 1) values.Add(_options[i, y].First());
+        }
+
+        //Set
+        int k = x / 3;
+        int l = y / 3;
+
+        for (int i = k * 3; i < (k + 1) * 3; i++)
+        {
+            for (int j = l * 3; j < (l + 1) * 3; j++)
+            {
+                if (_options[i, j].Count == 1) values.Add(_options[i, j].First());
+            }
+        }
+
+        values = values.Distinct().ToList();
+
+        _options[x, y] = new List<char>();
+        _options[x, y].AddRange(digits.Where(d => !values.Contains(d)));
+    }
+
+    public void CalculateOptions()
+    {
+        for (int i = 0; i < 9; i++)
+        {
+            for (int j = 0; j < 9; j++)
+            {
+                if (_options[i, j].Count != 1)
+                {
+                    CalculateCellOptions(i, j); 
+                }
+            }
+        }
+    }
+
+    public bool ValidateBoard()
+    {
+        var symbols = new HashSet<char>();
+
+        //Validate rows
+        for (int i = 0; i < 9; i++)
+        {
+            symbols.Clear();
+
+            for (int j = 0; j < 9; j++)
+            {
+                if (_options[i, j].Count == 1)
+                {
+                    if (symbols.Contains(_options[i, j][0])) return false;
+                    symbols.Add(_options[i, j][0]);
+                }
+            }
+        }
+
+        //Validate columns
+        for (int i = 0; i < 9; i++)
+        {
+            symbols.Clear();
+
+            for (int j = 0; j < 9; j++)
+            {
+                if (_options[j, i].Count == 1)
+                {
+                    if (symbols.Contains(_options[j, i][0])) return false;
+                    symbols.Add(_options[j, i][0]);
+                }
+            }
+        }
+
+        //Validate sets
+        for (int i = 0; i < 3; i++)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                symbols.Clear();
+
+                for (int k = 0; k < 3; k++)
+                {
+                    for (int l = 0; l < 3; l++)
+                    {
+                        if (_options[i * 3 + k, j * 3 + l].Count == 1)
+                        {
+                            if (symbols.Contains(_options[i * 3 + k, j * 3 + l][0])) return false;
+                            symbols.Add(_options[i * 3 + k, j * 3 + l][0]);
+                        }
+                    }
+                }                
+            }
+        }
+
+        return true;
+    }
+
+    public List<char> GetOptions(int x, int y) => _options[x, y];
+
+    public void SetOption(int x, int y, char value)
+    {
+        _options[x, y] = new List<char> { value };
+    }
+
+    public void DisplayBoard()
+    {
+        for (int i = 0; i < 9; i++)
+        {
+            for (int j = 0; j < 9; j++)
+            {
+                if (_options[i, j].Count == 1) Console.Write(_options[i, j][0]);
+                else Console.Write('.');
+                Console.Write(' ');
+            }
+            Console.WriteLine();
+        }
+        if (!ValidateBoard())
+        {
+            Console.WriteLine("Board invalid!");
+        }
+        Console.WriteLine();
+    }
+
+    public void CheckBoard()
+    {
+        DisplayBoard();
+        CalculateOptions();
+    }
+}
